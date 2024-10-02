06c9fffa42e4d38e5e039a93d5f82b6d5b790fbf
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-02 23:10:31 +0200

    Added logic for env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,23 @@
+/*
+* implement function that parses environment variables
+* with prefix RSS_ and prints them to the console in
+* the format RSS_name1=value1; RSS_name2=value2
+ */
+
 const parseEnv = () => {
-    // Write your code here 
+	try {
+		const prefix = 'RSS_';
+		if (!process.argv.some(value => value.startsWith(prefix))) {
+			throw Error('No variable here');
+		}
+		process.argv.forEach(variable => {
+			if (variable.startsWith(prefix)) {
+				console.log(variable);
+			}
+		});
+	}  catch (err) {
+		console.error(err);
+	}
 };
 
 parseEnv();
