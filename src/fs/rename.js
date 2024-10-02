44f0f7e2a1eca0aa4db83e458660ca2f8c81a1f3
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-02 22:50:47 +0200

    Added logic for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+
+/*
+* implement function that renames file wrongFilename.txt to properFilename with extension
+* .md (if there's no file wrongFilename.txt or properFilename.md already exists Error
+* with message FS operation failed must be thrown)
+ */
+
 const rename = async () => {
-    // Write your code here 
+	  const fileToRename = './files/wrongFilename.txt';
+		const newFileName = './files/properFilename.md';
+    try {
+	    if (fs.existsSync(newFileName) || !fs.existsSync(fileToRename)) {
+		    throw Error('FS operation failed');
+	    }
+	    fs.renameSync(fileToRename,newFileName);
+	    console.log('File renamed successfully!');
+    } catch (error) {
+	    console.log(error);
+    }
 };
 
 await rename();
