8dde0edc731c58d933b48893e7aa616f3cf226e8
Anhelina Mahidava <dinalla88@gmail.com>
2024-10-02 21:45:31 +0200

    Added logic for create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+
+/*
+* implement function that creates new file fresh.txt with content I am fresh and young
+* inside of the files folder (if file already exists Error with message FS operation
+* failed must be thrown)
+ */
+
 const create = async () => {
-    // Write your code here 
+	try {
+		if (fs.existsSync('./files/fresh.txt')) {
+			throw Error('FS operation failed');
+		}
+		const content = 'I am fresh and young';
+		fs.writeFile('./files/fresh.txt', content, err => {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log('File written successfully');
+			}
+		});
+	} catch (error){
+		console.error(error);
+	}
 };
 
 await create();
