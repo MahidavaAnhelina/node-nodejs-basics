80f4d0994ab483c479404f731d63152cc1f9fe9b
Anhelina Mahidava <dinalla88@gmail.com>
2024-10-02 22:36:34 +0200

    Added logic for copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+
+/*
+ * implement function that copies folder files files with all its content into folder files_copy at
+ * the same level (if files folder doesn't exists or files_copy has already been created Error with
+ * message FS operation failed must be thrown)
+ */
+
 const copy = async () => {
-    // Write your code here 
+	const destinationName = './files_copy';
+	const srcName = 'files';
+	try {
+		if (fs.existsSync(destinationName) || !fs.existsSync(srcName)) {
+			throw Error('FS operation failed');
+		}
+		fs.cpSync(srcName, destinationName, {recursive: true});
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 await copy();
