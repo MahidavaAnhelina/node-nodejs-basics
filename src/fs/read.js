1c41139ad480928f7cb698f9b2545afc6b5218a8
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-02 23:01:15 +0200

    Added logic for read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+
+/*
+* implement function that prints content of the fileToRead.txt
+* into console (if there's no file fileToRead.txt Error with
+* message FS operation failed must be thrown)
+ */
+
 const read = async () => {
-    // Write your code here 
+	const fileToRead = './files/fileToRead.txt';
+	try {
+		if (!fs.existsSync(fileToRead)) {
+			throw Error('FS operation failed');
+		}
+		const data = fs.readFileSync(fileToRead, 'utf8');
+		console.log(data);
+		console.log('File read successfully!');
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 await read();
