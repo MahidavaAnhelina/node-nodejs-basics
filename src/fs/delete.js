7110c26e74a8afa5dbca9c14ecd0fd07cdd0469d
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-02 22:54:48 +0200

    Added logic for delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,29 @@
+/*
+* implement function that deletes file fileToRemove.txt
+* (if there's no file fileToRemove.txt Error with message
+* FS operation failed must be thrown)
+ */
+
+import fs from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+	try {
+		const fileToRemove = './files/fileToRemove.txt';
+		if (!fs.existsSync(fileToRemove)) {
+			throw Error('FS operation failed');
+		}
+
+		fs.unlink(fileToRemove, (err) => {
+			if (err) {
+				console.error(`Error removing file: ${err}`);
+				return;
+			}
+
+			console.log(`File ${fileToRemove} has been successfully removed.`);
+		});
+	} catch (error){
+		console.error(error);
+	}
 };
 
 await remove();
