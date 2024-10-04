227efe213942dc70d9701ed4615634ce28e9b38a
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-04 15:58:25 +0200

    Added logic for calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+/*
+* implement function that calculates SHA256 hash for file
+* fileToCalculateHashFor.txt and logs it into console as
+* hex using Streams API
+ */
+
+import crypto from 'node:crypto';
+import fs from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+	const hash = crypto.createHash('sha256');
+	const rs = fs.createReadStream('./files/fileToCalculateHashFor.txt');
+	rs.on('error', err => console.log(err));
+	rs.on('data', chunk => hash.update(chunk));
+	rs.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
