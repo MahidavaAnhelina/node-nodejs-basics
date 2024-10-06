c4eb6f9a5e3f283c451e7e801086f5d5e7bf5864
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-06 21:29:42 +0300

    Added logic for main.js and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import {Worker} from "node:worker_threads";
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+
+	const numberCores = os.availableParallelism();
+
+	let numberToUse = 10;
+	const lisWorkers = [];
+
+	for (let i = 0; i < numberCores; i++) {
+		lisWorkers.push(new Promise(resolve => {
+			const worker = new Worker('./worker.js', {workerData: {n: ++numberToUse}});
+
+			worker.on('message', (message) => {
+				resolve({
+					status: 'resolved',
+					data: message
+				});
+			});
+
+			worker.on('error', (error) => {
+				resolve({
+					status: 'error',
+					data: null
+				});
+			});
+
+			worker.on('exit', (code) => {
+				if (code !== 0) console.error(`Worker stopped with exit code ${code}`);
+			});
+		}));
+	}
+
+	const result = await Promise.all(lisWorkers);
+	console.log(result);
 };
 
 await performCalculations();
