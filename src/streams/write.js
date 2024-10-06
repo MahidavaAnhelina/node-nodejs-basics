473817df6320ac911a1d83acd1ebf10e1093cabe
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-06 12:39:14 +0300

    Added logic for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,44 @@
+/*
+* implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+ */
+
+import readline from 'node:readline';
+
+import fs from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+	try {
+		const ws = fs.createWriteStream('./files/fileToWrite.txt');
+
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+
+
+		rl.on('line', (input) => {
+			ws.write(input + '\n');
+		});
+
+		rl.on('SIGINT', () => {
+			rl.question('Are you sure you want to exit? (Yes/No)', (answer) => {
+				if (answer.match(/^y(es)?$/i)) {
+					rl.close();
+					ws.end();
+				}
+			});
+		});
+
+		ws.on('finish', () => {
+			console.log('Data written to file successfully.');
+		});
+
+		ws.on('error', (err) => {
+			console.error('Error writing to file:', err.message);
+		});
+	} catch (err) {
+		console.log(err.message);
+	}
 };
 
 await write();
