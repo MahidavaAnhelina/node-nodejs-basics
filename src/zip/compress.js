81d8476b5b1da8140362cec45591c4f5829341fd
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-06 13:11:35 +0300

    Added logic for compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+/*
+* implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+ */
+
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+	const source = createReadStream('./files/fileToCompress.txt');
+	const gzip = createGzip();
+	const destination = createWriteStream('./files/archive.gz');
+
+	pipeline(source, gzip, destination, (err) => {
+		if (err) {
+			console.error('An error occurred:', err);
+			process.exitCode = 1;
+		}
+	});
 };
 
 await compress();
