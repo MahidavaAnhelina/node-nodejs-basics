3c5651b2d0732c264b1d9f37e802fc17d09a6975
Anhelina Mahidava <mahidavaanhelina@gmail.com>
2024-10-06 13:18:52 +0300

    Added logic for decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+/*
+* implement function that decompresses archive.gz back
+* to the fileToCompress.txt with same content as before
+* compression using zlib and Streams API
+ */
+
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+import { pipeline } from "node:stream";
+
 const decompress = async () => {
-    // Write your code here 
+	const source = createReadStream('./files/archive.gz');
+	const unzip = createUnzip();
+	// File name changed - to compare with initial file
+	const destination = createWriteStream('./files/fileToCompress1.txt');
+
+	pipeline(source, unzip, destination, (err) => {
+		if (err) {
+			console.error('An error occurred:', err);
+			process.exitCode = 1;
+		}
+	});
 };
 
 await decompress();
